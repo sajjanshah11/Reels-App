abc46014a0c8454862f6ee2c74965816a216f768
Sajjan <iamsajjanshahwork@gmail.com>
2021-08-21 15:30:14 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrEXVuKV-fvM--ULatELX9EVJZ5uWnYSE",
+    authDomain: "reels-app-cc387.firebaseapp.com",
+    projectId: "reels-app-cc387",
+    storageBucket: "reels-app-cc387.appspot.com",
+    messagingSenderId: "109908015394",
+    appId: "1:109908015394:web:c16eed00f3819ffbe38d7b"
+  };
+
+  firebase.initializeApp(firebaseConfig)
